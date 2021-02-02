84b86be24aac4e8bb3983e049e06a2ee7caa30c6
danleks <danilevich.lks@gmail.com>
2021-02-02 21:40:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const container = document.querySelector('.beers-container');
 const pagination = document.querySelector('.pagination');
 const paginationItems = document.querySelectorAll('.pagination__item');
+const itemsPerPageSelect = document.querySelector('#itemsPerPage-select');
+const sortSelect = document.querySelector('#sort-select');
+
+// state vars
+let itemsPerPage = 3;
+
 
 const render = (beers) => {
     if (container.children.length > 0) {
@@ -22,21 +28,39 @@ const render = (beers) => {
     container.appendChild(fragment);
 }
 
-const handleBeers = async (pageNum = 1) => {
-    const BEERS_API = `https://api.punkapi.com/v2/beers?page=${pageNum}&per_page=3`;
+const handleBeers = async (pageNum = 1, showItems = 3, sortByValue = 'abv') => {
+    const BEERS_API = `https://api.punkapi.com/v2/beers?page=${pageNum}&per_page=${showItems}`;
     let response = await fetch(BEERS_API);
     let beers = await response.json();
-    render(beers);
+    const beersSorted = beers.sort((a,b) => a[sortByValue] - b[sortByValue]);
+    console.log(beers);
+    render(beersSorted);
 
     return beers;
 }
 
-handleBeers();
-
-const handleClick = (e) => {
-    if (e.target.tagName !== "LI") return;
-    let pageNumber = +e.target.textContent;
-    handleBeers(pageNumber);
+const handleItemsPerPage = (e) => {
+    if (e) itemsPerPage = +e.target.value;
+    handleBeers(undefined, itemsPerPage);
 };
 
-pagination.addEventListener('click', handleClick );
+const handleSort = (e) => {
+    console.log(e.target.value);
+    let sortByValue = e.target.value;
+    handleBeers(undefined, itemsPerPage, sortByValue)
+}
+
+const handlePagination = (e) => {
+    if (e.target.tagName !== "LI") return;
+    let pageNumber = +e.target.textContent;
+    handleBeers(pageNumber, itemsPerPage);
+};
+
+handleBeers();
+//handleItemsPerPage(undefined, itemsPerPage);
+
+itemsPerPageSelect.addEventListener('change', handleItemsPerPage);
+sortSelect.addEventListener('change', handleSort);
+
+
+pagination.addEventListener('click', handlePagination );
